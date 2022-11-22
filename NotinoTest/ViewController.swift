9836a3f051c6533e91761e37d3c634726a714f15
ermolenkoA <kypalo2012@gmail.com>
2022-11-22 14:13:30 +0300

    Added navigation bar and collection view

--- NotinoTest/ViewController.swift
+++ NotinoTest/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    private var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 0.94)
+        title = "Produkty"
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.alwaysBounceVertical = true
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints { maker in
+            maker.trailing.top.bottom.leading.equalTo(view.safeAreaLayoutGuide)
+        }
     }
-
-
+    
+    
 }
 
+extension ViewController: UICollectionViewDelegate {
+    
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+}
--- Podfile
+++ Podfile
@@ -5,10 +5,8 @@ target 'NotinoTest' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-pod 'SwiftLint', '0.48.0'
 pod 'SnapKit', '5.6.0'
 pod 'Kingfisher', '7.3.2'
-
 
   # Pods for NotinoTest
 
